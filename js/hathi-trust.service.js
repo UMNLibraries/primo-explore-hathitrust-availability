71092645a60f58f57ed660888a8bce7d13a2b3b3
gpeterso <gpeterso@umn.edu>
2016-12-14 13:50:05 -0600

    Refactor service
    Move the response handling into a separate callback function.
    Also move this controller and component definitions into the same
    file. These are tightly coupled and best understood together.

--- js/hathi-trust.service.js
+++ js/hathi-trust.service.js
@@ -3,27 +3,33 @@ app.factory('hathiTrust', ['$http', '$q', function ($http, $q) {
   var hathiTrustBaseUrl = "https://catalog.hathitrust.org/api/volumes/brief/json/";
 
   svc.findFullViewRecord = function (ids) {
-    var hathiTrustFullTextRecord = false;
     var deferred = $q.defer();
-    if (ids.length) {
-      var hathiTrustLookupUrl = hathiTrustBaseUrl + ids.join('|') + "?callback=JSON_CALLBACK";
-      $http.jsonp(hathiTrustLookupUrl, { cache: true }).success(function (data) {
-        for (var i = 0; !hathiTrustFullTextRecord && i < ids.length; i++) {
-          var result = data[ids[i]];
-          for (var j = 0; j < result.items.length; j++) {
-            var item = result.items[j];
-            if (item.usRightsString.toLowerCase() == "full view") {
-              hathiTrustFullTextRecord = result.records[item.fromRecord].recordURL;
-              break;
-            }  
+
+    var handleResponse = function(data) {
+      var fullTextUrl = null;
+      for (var i = 0; !fullTextUrl && i < ids.length; i++) {
+        var result = data[ids[i]];
+        for (var j = 0; j < result.items.length; j++) {
+          var item = result.items[j];
+          if (item.usRightsString.toLowerCase() === "full view") {
+            fullTextUrl = result.records[item.fromRecord].recordURL;
+            break;
           }  
         }  
-        deferred.resolve(hathiTrustFullTextRecord);
-      });
+      }  
+      deferred.resolve(fullTextUrl);
+    }
+
+    if (ids.length) {
+      var hathiTrustLookupUrl = hathiTrustBaseUrl + ids.join('|') + "?callback=JSON_CALLBACK";
+      $http.jsonp(hathiTrustLookupUrl, { cache: true }).success(handleResponse);
     } else {
-      deferred.resolve(false);
+      deferred.resolve(null);
     }  
+
     return deferred.promise;
   }; 
+
   return svc;
+
 }]);
--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,6 @@ module.exports = function(config) {
       './test/hathi-trust-availability.controller.spec.js',
       './js/custom.module.js', 
       './js/hathi-trust.service.js', 
-      './js/hathi-trust-availability.controller.js', 
       './js/hathi-trust-availability.component.js', 
 
       { pattern: 'test/fixtures/*.json', watched: true, served: true, included: false }
