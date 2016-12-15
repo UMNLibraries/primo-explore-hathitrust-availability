eb376acdf44d255c4959741d34e4e3536cc50a0d
gpeterso <gpeterso@umn.edu>
2016-12-15 15:41:53 -0600

    Initial commit for public repo

---
+++ README.md
@@ -0,0 +1,45 @@
+# primo-explore-hathitrust-availability
+An example implementation of a custom Angular directive in the new Primo UI
+
+## Purpose
+During the [Primo Hackathon](http://igelu.org/archives/9618), we started discussing best practices for sharing custom Primo components / directives via NPM packages. This is not intended to serve as a definitive list of best practices. Rather, the goal is to seed the conversation with some concrete examples of how we might: 
+  - avoid overwriting the built-in 'after' components by defining child components that can be appended to an 'after' component.
+  - move external API calls out of controllers and into custom services
+  - create unit tests for services and component controllers 
+
+## Features
+When search results are displayed, a record's OCLC numbers are passed to the [HathiTrust Bib API](https://www.hathitrust.org/bib_api). If at least one item with free full-text access is found, a link to the HathiTrust record is appended to the availability section. 
+
+### Screenshot
+![screenshot](screenshots/screenshot.png)
+
+## Usage
+Once the module is installed in your [customization environment](https://github.com/ExLibrisGroup/primo-explore-devenv), you can add the HathiTrust availability component to the `prmSearchResultAvailabilityLineAfter` component like so: 
+
+```javascript
+app.component('prmSearchResultAvailabilityLineAfter', { 
+  bindings: { parentCtrl: '<'},
+  template: '<hathi-trust-availability></hathi-trust-availability>'
+});
+```
+The idea here is to allow multiple custom components to be added to the `prmSearchResultAvailabilityLineAfter` rather than attempting to redefine it. 
+
+By default, the component will display full-text links for any resource. If you want it avoid looking for full-text availability on records for which you already have an online copy, you can add a `hide-online=tue` attribute to the component: 
+
+```html
+<hathi-trust-availability hide-online="true"></hathi-trust-availability>
+```
+
+The default availability message is "Full Text Available at HathiTrust". You can override this by setting the `msg` attribute:
+
+```html
+<hathi-trust-availability hide-online="true" msg="WOW, HathiTrust! Lucky you!"></hathi-trust-availability>
+```
+
+## Running Tests
+1. Clone the repo
+2. Run `npm install`
+3. Run `npm test`
+
+
+
---
+++ test/hathi-trust-availability.controller.spec.js
@@ -0,0 +1,73 @@
+describe('hathiTrustAvailabilityController', function(){
+
+  beforeEach(module('viewCustom'));
+  var $componentController, $rootScope, $q, hathiTrust, ctrl, parentCtrl, expectedIds, bindings;
+
+  beforeEach(inject(function(_$componentController_, _$rootScope_, _hathiTrust_, $injector){
+    $componentController = _$componentController_;
+    $rootScope = _$rootScope_;
+    hathiTrust = _hathiTrust_;
+    $timeout = $injector.get('$timeout');
+    $q = $injector.get('$q');
+    expectedIds = ["oclc:1586310", "oclc:7417753", "oclc:47076528"];
+    parentCtrl = {};
+    parentCtrl.result = getJSONFixture('print_result.json');
+    bindings = {hathiTrust: hathiTrust, 
+                parent: {parentCtrl: parentCtrl}};
+
+  }));
+
+  it('should pass the OCLC numbers to the hathiTrust service', function(){
+    spyOn(hathiTrust, 'findFullViewRecord').and.
+      returnValue({then: function(callback){ return callback(true)}});
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(hathiTrust.findFullViewRecord).toHaveBeenCalledWith(expectedIds);
+  });
+
+  it('should update the hathiTrustFullText link if available', function(){
+    var link = "http://example.com";
+    spyOn(hathiTrust, 'findFullViewRecord').and.
+      returnValue({then: function(callback){ return callback(link)}});
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(ctrl.fullTextLink).toBe(link);
+  });
+
+  it('should not call the hathTrust service for online resoureces when disabled', function(){
+    parentCtrl.result = getJSONFixture('online_result.json');
+    spyOn(hathiTrust, 'findFullViewRecord').and.
+      returnValue({then: function(callback){ return callback(true)}});
+
+    bindings.hideOnline = true;
+
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(hathiTrust.findFullViewRecord).not.toHaveBeenCalled();
+   });
+
+  it('should call the hathTrust service for online resoureces by default', function(){
+    parentCtrl.result = getJSONFixture('online_result.json');
+    spyOn(hathiTrust, 'findFullViewRecord').and.
+      returnValue({then: function(callback){ return callback(true)}});
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(hathiTrust.findFullViewRecord).toHaveBeenCalled();
+  });
+
+  it('should accept a custom availability message', function(){
+    var myMsg = "FULL TEXT FROM HATHITRUST, YAY!";
+    bindings.msg = myMsg;
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(ctrl.msg).toBe(myMsg);
+  });
+
+  it('should use a default availability message when not provided', function(){
+    var expectedDefaultMsg = 'Full Text Available at HathiTrust';
+    ctrl = $componentController('hathiTrustAvailability', null, bindings);
+    ctrl.$onInit();
+    expect(ctrl.msg).toBe(expectedDefaultMsg);
+  });
+
+});
