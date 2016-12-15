1c1ea54a68dcebd6bb57c06cb8934ef2e7593169
gpeterso <gpeterso@umn.edu>
2016-12-15 15:26:30 -0600

    Use one-time binding with msg

--- js/hathi-trust-availability.component.js
+++ js/hathi-trust-availability.component.js
@@ -43,7 +43,7 @@ app.component('hathiTrustAvailability', {
   controller: 'hathiTrustAvailabilityController',
   template: '<span class="umnHathiTrustLink">\
               <a target="_blank" ng-if="$ctrl.fullTextLink" ng-href="{{$ctrl.fullTextLink}}">\
-              {{$ctrl.msg}}\
+              {{ ::$ctrl.msg }}\
                 <prm-icon external-link="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
               </a>\
             </span>'
