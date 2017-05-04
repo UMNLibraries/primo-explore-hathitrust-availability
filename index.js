f816c5470fee37a520419638d1716592a96757e1
gpeterso <gpeterso@umn.edu>
2017-05-04 12:54:00 -0500

    Add module name to exports
    This should allow for nicer ES2015 import statements. e.g.
    `import HathTrust from 'primo-explore-hathitrust-availability'`

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 require('./js/hathi-trust-availability.module.js');
 require('./js/hathi-trust.service.js');
 require('./js/hathi-trust-availability.component.js');
+module.exports = 'hathiTrustAvailability';
