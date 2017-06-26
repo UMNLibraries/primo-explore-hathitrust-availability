3ca40cad77a0656bbbf3be76501010ded72488b0
gpeterso <gpeterso@umn.edu>
2017-06-26 11:15:27 -0500

    Remove ES6 stuff from module config block
    External modules (node_modules) are not transformed by default.
    More info here: https://github.com/babel/babelify#faq

--- js/hathi-trust-availability.module.js
+++ js/hathi-trust-availability.module.js
@@ -2,9 +2,9 @@ angular
   .module('hathiTrustAvailability', [])
   .value('iconSrc', 'custom/CENTRAL_PACKAGE/img/hathitrust.svg')
   .constant('hathiTrustBaseUrl', "https://catalog.hathitrust.org/api/volumes/brief/json/")
-  .config(['$sceDelegateProvider', 'hathiTrustBaseUrl', ($sceDelegateProvider, hathiTrustBaseUrl) => {
-    let urlWhitelist = $sceDelegateProvider.resourceUrlWhitelist();
-    urlWhitelist.push(`${hathiTrustBaseUrl}**`)
+  .config(['$sceDelegateProvider', 'hathiTrustBaseUrl', function($sceDelegateProvider, hathiTrustBaseUrl) {
+    var urlWhitelist = $sceDelegateProvider.resourceUrlWhitelist();
+    urlWhitelist.push(hathiTrustBaseUrl + '**')
     $sceDelegateProvider.resourceUrlWhitelist(urlWhitelist);
   }]);
 
