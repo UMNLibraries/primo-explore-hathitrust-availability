f7b4b7cd562cce18bb179e78e37ac661125af272
Jeff Peterson <gpeterso@umn.edu>
2022-05-17 14:08:45 -0500

    Update dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,11 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    plugins: ['@metahub/karma-jasmine-jquery', 'karma-*'],
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine-jquery', 'jasmine'],
+    frameworks: ['jasmine-jquery'],
 
 
     // list of files / patterns to load in the browser
